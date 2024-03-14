6474b63f09155a78d1b8584c585f1943cfaaee22
José Carlos <Cajosecarlosmendesdasilva@gmail.com>
2024-03-14 17:34:08 -0300

    ex em python

---
+++ soma_3valor_int_emminutos.py.py
@@ -0,0 +1,15 @@
+#Aqui neste codigo estou criando três variavel.
+#cada uma recebe numeros inteiro que representa tempo em minutos.("Linha,6,7,8")
+#depois uma varial (tempo), que armanzena a soma desses valores. ("Linha,10")
+#na linha 12 a função print, que imprime o Valor no teminal.
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+tempo = n1 + n2 + n3
+
+print ( tempo, 'minutos', )
+
+
+
